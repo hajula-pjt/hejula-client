b77e329a2c027d281a9fbdf386aa2761fc9edb6d
yoonhe <yhehe228@gmail.com>
2021-09-12 21:23:30 +0900

    Feat : [예약 확인 및 결제 페이지] 예약 정보 컴포넌트 생성 - 체크인, 체크아웃 날짜 표시 - 인원 표시 (성인 n명 + 어린이 n명 형태)

--- pages/reservationConfirm.tsx
+++ pages/reservationConfirm.tsx
@@ -1,16 +1,71 @@
+import styled from "@emotion/styled";
 import { useRouter } from "next/router";
 import { FC } from "react";
 
 import { Container } from "./style/layout";
 
+const isNotZero = (text: string) => {
+  return text !== "0";
+};
+
 const ReservationConfirm: FC = () => {
   const router = useRouter();
 
+  const {
+    accommodationSeq,
+    adultCount,
+    checkinDate,
+    checkoutDate,
+    childrenCount,
+  } = router.query;
+
   return (
     <Container>
-      <h2>예약 확인 및 결제 페이지</h2>
+      <PageTitle>확인 및 결제</PageTitle>
+      <section>
+        <SectionTitle>예약 정보</SectionTitle>
+        <Infomation>
+          <dt>날짜</dt>
+          <dd>
+            {checkinDate} ~ {checkoutDate}
+          </dd>
+          <dt>인원</dt>
+          <dd>
+            {isNotZero(adultCount) && <span>성인 {adultCount}명</span>}
+            {isNotZero(childrenCount) && (
+              <span> + 어린이 {childrenCount}명</span>
+            )}
+          </dd>
+        </Infomation>
+      </section>
     </Container>
   );
 };
+
+const PageTitle = styled.h2`
+  margin-bottom: 48px;
+  font-size: 30px;
+`;
+
+const SectionTitle = styled.h3`
+  margin-bottom: 24px;
+  font-size: 22px;
+`;
+
+const Infomation = styled.dl`
+  dt,
+  dd {
+    font-size: 16px;
+  }
+  dd {
+    color: #717171;
+    margin-top: 8px;
+    font-weight: 300;
+  }
+
+  dd + dt {
+    margin-top: 24px;
+  }
+`;
 
 export default ReservationConfirm;
